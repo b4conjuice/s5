1b0c1f0f1fe874b962a3c38752cdd3eaec6f1239
Daniel Lopez <d@niel.to>
2026-08-01 21:58:25 -0700

    feat: add select scripture url type modal

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,37 @@
+import { useState } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { Cog6ToothIcon } from '@heroicons/react/20/solid'
 
 import BookSearch from '@/components/book-search'
+import Modal from '@/components/ui/modal'
+import SelectScriptureUrlType from '@/components/book-search/components/select-scripture-url-type'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <main className='flex grow flex-col p-4'>
       <div className='flex grow flex-col gap-4'>
-        <h1 className='font-bold'>s5 🔍</h1>
+        <div className='flex justify-between'>
+          <h1 className='font-bold'>s5 🔍</h1>
+          <button
+            type='button'
+            onClick={() => {
+              setIsModalOpen(true)
+            }}
+            className='text-cb-yellow hover:text-cb-yellow/75'
+          >
+            <Cog6ToothIcon className='h-6 w-6' />
+          </button>
+          <Modal
+            isOpen={isModalOpen}
+            setIsOpen={setIsModalOpen}
+            title='where would you like to open scripture?'
+          >
+            <SelectScriptureUrlType />
+          </Modal>
+        </div>
         <div className='flex grow flex-col justify-end gap-4'>
           <BookSearch />
         </div>
