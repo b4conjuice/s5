2b9c2a9fd321fd142de76c5125ffb7d707189619
Daniel Lopez <d@niel.to>
2026-08-09 18:17:14 -0700

    feat(history): add button clear history

--- src/routes/history.tsx
+++ src/routes/history.tsx
@@ -1,19 +1,34 @@
+import { useState } from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { CheckIcon, TrashIcon, XMarkIcon } from '@heroicons/react/20/solid'
 
 import HistoryList from '@/components/history-list'
 import Menu from '@/components/menu'
+import useHistory from '@/components/book-search/hooks/use-history'
+import Modal from '@/components/ui/modal'
 
 export const Route = createFileRoute('/history')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
+  const { history, clearHistory } = useHistory() // TODO: use history in history-list
   return (
     <>
       <main className='flex grow flex-col p-4'>
         <div className='flex grow flex-col justify-between gap-4'>
-          <div className='flex justify-between'>
+          <div className='flex items-center space-x-4'>
             <h1 className='font-bold'>history</h1>
+            <button
+              className='text-red-700 hover:text-red-700/75'
+              type='button'
+              onClick={() => {
+                setIsConfirmModalOpen(true)
+              }}
+            >
+              <TrashIcon className='h-6 w-6' />
+            </button>
           </div>
           <div className='flex flex-grow flex-col justify-between space-y-4'>
             <HistoryList />
@@ -26,6 +41,29 @@ function RouteComponent() {
         </div>
         <div className='flex space-x-4'></div>
       </footer>
+      <Modal
+        isOpen={isConfirmModalOpen}
+        setIsOpen={setIsConfirmModalOpen}
+        title='are you sure you want to clear history?'
+      >
+        <div className='flex space-x-4'>
+          <button
+            onClick={() => {
+              clearHistory()
+              setIsConfirmModalOpen(false)
+            }}
+          >
+            <CheckIcon className='h-6 w-6' />
+          </button>
+          <button
+            onClick={() => {
+              setIsConfirmModalOpen(false)
+            }}
+          >
+            <XMarkIcon className='h-6 w-6' />
+          </button>
+        </div>
+      </Modal>
     </>
   )
 }
