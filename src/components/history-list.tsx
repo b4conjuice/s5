2f0741c0de7a1524a9d36856ddbb6a87b760fd64
Daniel Lopez <d@niel.to>
2026-08-10 18:38:10 -0700

    feat(history): add ability to delete specific history entries

--- src/components/history-list.tsx
+++ src/components/history-list.tsx
@@ -1,4 +1,5 @@
-import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
+import { useState } from 'react'
+import { ArrowTopRightOnSquareIcon, TrashIcon } from '@heroicons/react/20/solid'
 import { format } from 'date-fns'
 
 import useHistory from '@/components/book-search/hooks/use-history'
@@ -7,16 +8,27 @@ import {
   transformBibleParamToScripture,
 } from '@/components/book-search/lib'
 import useScriptureUrlType from '@/components/book-search/hooks/use-scripture-url-type'
+import ConfirmModal from '@/components/confirm-modal'
+import type { HistoryEntry } from '@/components/book-search/lib/types'
+
+type HistoryEntryToBeDeleted = HistoryEntry & {
+  scriptureAsString: string
+  index: number
+}
 
 export default function HistoryList({ limit }: { limit?: number }) {
-  const { history } = useHistory()
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
+  const [historyEntryToBeDeleted, setHistoryEntryToBeDeleted] =
+    useState<HistoryEntryToBeDeleted | null>(null)
+  const { history, setHistory } = useHistory()
   const { scriptureUrlType } = useScriptureUrlType()
   return (
     <>
       {history.length > 0 ? (
         <ul className='divide-cb-dusty-blue divide-y'>
           {[...(limit !== undefined ? history.slice(0, limit) : history)].map(
-            ({ bibleParam, date }, index) => {
+            (historyEntry, index) => {
+              const { bibleParam, date } = historyEntry
               const scripture = transformBibleParamToScripture(bibleParam)
               if (scripture === '') {
                 return null
@@ -51,6 +63,20 @@ export default function HistoryList({ limit }: { limit?: number }) {
                     >
                       <ArrowTopRightOnSquareIcon className='h-6 w-6' />
                     </a>
+                    <button
+                      className='text-red-700 hover:text-red-700/75'
+                      type='button'
+                      onClick={() => {
+                        setHistoryEntryToBeDeleted({
+                          ...historyEntry,
+                          scriptureAsString: scripture.asString ?? '',
+                          index,
+                        })
+                        setIsConfirmModalOpen(true)
+                      }}
+                    >
+                      <TrashIcon className='h-6 w-6' />
+                    </button>
                   </div>
                 </li>
               )
@@ -60,6 +86,25 @@ export default function HistoryList({ limit }: { limit?: number }) {
       ) : (
         <p>no history</p>
       )}
+      {historyEntryToBeDeleted !== null ? (
+        <ConfirmModal
+          isOpen={isConfirmModalOpen}
+          setIsOpen={setIsConfirmModalOpen}
+          title='are you sure you want remove this history entry?'
+          action={() => {
+            const newHistory = [...history]
+            newHistory.splice(historyEntryToBeDeleted.index, 1)
+            setHistory(newHistory)
+            setIsConfirmModalOpen(false)
+            setHistoryEntryToBeDeleted(null)
+          }}
+        >
+          <div>{historyEntryToBeDeleted.scriptureAsString}</div>
+          <div className='text-cb-white/50 text-sm'>
+            {format(historyEntryToBeDeleted.date, 'MMM d, yyyy')}
+          </div>
+        </ConfirmModal>
+      ) : null}
     </>
   )
 }
